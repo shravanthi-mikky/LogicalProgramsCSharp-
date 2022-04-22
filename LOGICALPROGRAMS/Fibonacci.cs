8ea76ec0868b3a4538995c2dda79b61037e26aab
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-22 13:13:28 +0530

    Fibonacci series

---
+++ LOGICALPROGRAMS/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGICALPROGRAMS
+{
+    public class Fibonacci
+    {
+        public void FibonacciSeries()
+        {
+            Console.WriteLine("Enter the Number : ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3=0;
+            Console.WriteLine(n2);
+            Console.Write("Fibonacci Series of "+num+" is "+ n1 +" "+ n2 + " ");
+            for (int i = 0; n3 < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3+" ");
+                n1 = n2;
+                n2 = n3;
+             
+            }
+            Console.WriteLine("\n ");
+        }
+    
+    }
+}
--- LOGICALPROGRAMS/Program.cs
+++ LOGICALPROGRAMS/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("---------Logical Programs in C #-----------");
+using LOGICALPROGRAMS;
 
+Console.WriteLine("---------Logical Programs in C #-----------");
+while(true)
+{
+    Console.WriteLine("Enter the option to choose the program : \n1)Fibonacci Series");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            Console.WriteLine("Fibonacci Series");
+            Fibonacci fibonacci = new();
+            fibonacci.FibonacciSeries();
+            break;
+        default:
+            break;
+    }
+}
