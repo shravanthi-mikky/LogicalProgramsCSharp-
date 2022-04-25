dbdac2d3f4da1e0acded4563add3c418826a41c6
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-25 10:39:47 +0530

    Coupon Code

--- LOGICALPROGRAMS/StopWatch.cs
+++ LOGICALPROGRAMS/StopWatch.cs
@@ -13,17 +13,26 @@ namespace LOGICALPROGRAMS
         {
             Stopwatch stopwatch = new Stopwatch();
 
-            // Begin timing.
-            stopwatch.Start();
+            Console.WriteLine("Choose the option 1 to start : ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            if (start == 1)
+            {
+                // Begin timing.
+                stopwatch.Start();
+            }
 
             // Do something.
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Thread.Sleep(1);
             }
-
-            // Stop timing.
-            stopwatch.Stop();
+            Console.WriteLine("Choose the option 2 to start : ");
+            int stop = Convert.ToInt32(Console.ReadLine());
+            if (stop == 2)
+            {
+                // Stop timing.
+                stopwatch.Stop();
+            }
 
             // Write result.
             Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
